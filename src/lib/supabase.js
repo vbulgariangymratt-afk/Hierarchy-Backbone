c2f66cce438a1ef100d25c44ed53ca1a4da38155
Warhead <vbulgariangymratt@gmail.com>
2026-04-23 13:47:24 -0600

    fix safari oauth with pkce flow

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -11,7 +11,7 @@ if (!supabaseUrl || !supabaseAnonKey) {
 export const supabase = createClient(supabaseUrl || '', supabaseAnonKey || '', {
     auth: {
         flowType: "pkce",
-        detectSessionInUrl: false,
+        detectSessionInUrl: true,
         persistSession: true,
         autoRefreshToken: true
     }
